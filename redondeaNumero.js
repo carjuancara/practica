cb206020e2f54903194b3b59e65a2c9236a06378
Carlos Juan <carjuancara@gmail.com>
2021-12-31 12:32:05 -0300

    se modifico el metodo

--- redondeaNumero.js
+++ redondeaNumero.js
@@ -1,7 +1,6 @@
 // Redondear un numero a 2 decimales
 var numero = 23.2528293;
-var numero2decimal = 0;
-numero2decimal=numero.toFixed(2);
 
-console.log('El numero original es: ',numero);
-console.log('El numero redondeado a 2 decimales es: ',numero2decimal);
+
+console.log('El numero original es: ' + numero);
+console.log('El numero redondeado a 2 decimales es: ' + numero.toFixed(2));
