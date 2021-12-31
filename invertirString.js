8a563cbf449f9bd98310fbb633ce551bf96b48e7
Carlos Juan <carjuancara@gmail.com>
2021-12-31 12:20:36 -0300

    agregue descripcion metodo

--- invertirString.js
+++ invertirString.js
@@ -1,10 +1,16 @@
-// invertir string
+/* invertir string
+ dado un string, este metodo devolvera un nuevo string invirtiendo el 
+ orden de los elementos, el ultimo elemento a la primera posicion,
+el penultimo al segundo, etc.
+    stringOrigin   = 'Carlos'
+    stringDevuelto = 'solraC'
+ */
 
 
 var miString = 'Carlos Juan';
-var zxc= '';
+
 String.prototype.invertir = function () {
-    
+    let zxc= '';    
     for (var i = miString.length -1; i >= 0; i--) {
         zxc += miString[i];
     }
